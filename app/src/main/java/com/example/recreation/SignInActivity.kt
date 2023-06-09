bcf48a2773e2bfccf1ca79f17ded0af5218fed80
MrKot007 <arseniy.bobovnikov@gmail.com>
2023-06-09 23:04:59 +0300

    initialize

---
+++ app/src/main/java/com/example/recreation/SignInActivity.kt
@@ -0,0 +1,15 @@
+package com.example.recreation
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.recreation.databinding.ActivitySignInBinding
+
+class SignInActivity : AppCompatActivity() {
+    private lateinit var binding: ActivitySignInBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivitySignInBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+    }
+}
