cdc881999b946f6580aa6e05cf1cd8ac26bd17a9
MrKot007 <arseniy.bobovnikov@gmail.com>
2023-06-11 15:06:50 +0300

    shared

--- app/src/main/java/com/example/recreation/SharedPref.kt
+++ app/src/main/java/com/example/recreation/SharedPref.kt
@@ -10,27 +10,36 @@ class SharedPref {
             val editor = sp.edit()
             editor.putString("email", email).apply()
         }
-        fun getEmail(context: Context) {
+        fun getEmail(context: Context) : String?{
             val sp: SharedPreferences = context.getSharedPreferences("main", Context.MODE_PRIVATE)
-            sp.getString("email", "no value assigned")
+            return sp.getString("email", "no value assigned")
         }
         fun savePassword(password: String, context: Context) {
             val sp: SharedPreferences = context.getSharedPreferences("main", Context.MODE_PRIVATE)
             val editor = sp.edit()
             editor.putString("password", password).apply()
         }
-        fun getPassword(context: Context) {
+        fun getPassword(context: Context) : String? {
             val sp: SharedPreferences = context.getSharedPreferences("main", Context.MODE_PRIVATE)
-            sp.getString("password", "no value assigned")
+            return sp.getString("password", "no value assigned")
         }
         fun saveToken(token: String, context: Context) {
             val sp: SharedPreferences = context.getSharedPreferences("main", Context.MODE_PRIVATE)
             val editor = sp.edit()
             editor.putString("token", token).apply()
         }
-        fun getToken(context: Context) {
+        fun getToken(context: Context) : String?{
             val sp: SharedPreferences = context.getSharedPreferences("main", Context.MODE_PRIVATE)
-            sp.getString("token", "no value assigned")
+            return sp.getString("token", "no value assigned")
+        }
+        fun saveNotFirstEnter(context: Context) {
+            val sp: SharedPreferences = context.getSharedPreferences("main", Context.MODE_PRIVATE)
+            val editor = sp.edit()
+            editor.putBoolean("isFirstEnter", false).apply()
+        }
+        fun checkFirstEnter(context: Context) : Boolean {
+            val sp: SharedPreferences = context.getSharedPreferences("main", Context.MODE_PRIVATE)
+            return sp.getBoolean("isFirstEnter", true)
         }
     }
 }
--- app/src/main/java/com/example/recreation/SplashActivity.kt
+++ app/src/main/java/com/example/recreation/SplashActivity.kt
@@ -5,14 +5,32 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
+import com.example.recreation.Connection.api
 
 class SplashActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
-        Handler(Looper.getMainLooper()).postDelayed({
+        if (SharedPref.checkFirstEnter(this@SplashActivity)) {
+            SharedPref.saveNotFirstEnter(this@SplashActivity)
             startActivity(Intent(this@SplashActivity, SignUpActivity::class.java))
             finish()
-        }, 1500)
+        }else {
+            api.signIn(ModelAuth(SharedPref.getEmail(this@SplashActivity)!!, SharedPref.getPassword(this@SplashActivity)!!))
+                .push(object: OnGetData<ModelIdentity>{
+                    override fun onGet(data: ModelIdentity) {
+                        startActivity(Intent(this@SplashActivity, MainActivity::class.java))
+                        finish()
+                    }
+
+                    override fun onError(error: String) {
+                        Handler(Looper.getMainLooper()).postDelayed({
+                            startActivity(Intent(this@SplashActivity, SignInActivity::class.java))
+                            finish()
+                        }, 1500)
+                    }
+                }, this)
+        }
+
     }
 }
